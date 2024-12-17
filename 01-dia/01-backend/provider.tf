7585749a89d01393dba3fa40b4bf4b2dc466ebc3
Rodrigo Corazza <rodrigo.corazza@gmail.com>
2024-12-17 11:10:50 -0300

    add files

---
+++ 01-dia/01-backend/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "descomplicando-terraform-corazza"
+    key    = "aula_backend"
+    region = "us-east-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
